907d7b5c3ce70b38062ce13f0f7c0f2ef4e386da
Victor Fridal <cljvic2020@gmail.com>
2023-06-13 16:07:41 +0100

    Add band name file

---
+++ DAY 1/band_name_generator.py
@@ -0,0 +1,6 @@
+print("Hello! welcome to Melband")
+city = input("Enter the city you grew up in?: \n ")
+pet = input("what is the name of your pet?: \n ")
+band_name = city + " " + pet 
+
+print("Hello! your new bandname is " + band_name)
