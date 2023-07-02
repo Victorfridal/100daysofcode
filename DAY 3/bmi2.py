87c4012fed53da5eb545d729e2c53b113fa11226
Victor Fridal <cljvic2020@gmail.com>
2023-07-02 18:39:44 +0100

    bmi2

--- DAY 3/bmi2.py
+++ DAY 3/bmi2.py
@@ -1,19 +1,14 @@
 height = float(input("what is your height in m: "))
 weight = float(input("what is your weight in kg: "))
-BMI =   round(float(weight / height ** 2),2)
+BMI = round(float(weight / height ** 2),0)
+print(BMI)
 if BMI <= 18.5:
-    print("you are underweight ")
-    if BMI >= 18.5:
-        print("you have a normal weight ")
-    elif BMI <= 25:
-        print("you have a normal weight ")
-    if BMI >=25:
-        print("you are overweight ")
-    elif BMI <=30:
-        print("you are overweight ")
-    if BMI >= 30:
-        print("you are obese")
-    elif BMI <=35:
-        print("you are obese")
-    if BMI >= 35:
-        print("you are clinically obese ")
+    print(f"your BMI is {BMI}, you are underweight ")
+elif BMI <= 25:
+    print(f"your BMI is {BMI}, you have a normal weight ")
+elif BMI <=30:
+    print(f"your BMI is {BMI}, you are overweight ")
+elif BMI <=35:
+    print(f"your BMI is {BMI}, you are obese ")
+else:
+    print(f"your BMI is {BMI}, you are clinically obese ")
